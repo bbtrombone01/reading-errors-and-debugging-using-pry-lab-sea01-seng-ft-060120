f2ecdeec49cae84176d5c2560b0ffecc64aa1ec4
steven <bbtrombone@sbcglobal.net>
2020-04-18 00:24:28 +0000

    Automatically backed up by Learn

--- spec/the_final_frontier_spec.rb
+++ spec/the_final_frontier_spec.rb
@@ -3,6 +3,7 @@ require_relative '../fix_using_tests/the_final_frontier'
 describe "the_final_frontier" do
   it "`engage` should return the captain's log and crew greetings" do
     expect{ engage }.to output(/Captain's Log, star date /).to_stdout
-    puts engage
+    date = "this"
+    puts date
   end
 end
